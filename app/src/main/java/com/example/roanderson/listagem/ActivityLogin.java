a0281b57839cc394c46b67495c5bac6633e227d6
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2016-09-02 18:00:30 -0300

    botao pega cpf sem . e -

--- app/src/main/java/com/example/roanderson/listagem/ActivityLogin.java
+++ app/src/main/java/com/example/roanderson/listagem/ActivityLogin.java
@@ -3,6 +3,8 @@ package com.example.roanderson.listagem;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import Util.Criptografia;
@@ -12,6 +14,8 @@ public class ActivityLogin extends AppCompatActivity {
     String cpfCripto;
     private EditText edtCpf;
     private MaskEditTextChangedListener maskCPF;
+    private Button botao;
+    private String cpfAux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,22 @@ public class ActivityLogin extends AppCompatActivity {
         setSupportActionBar(toolbar);
         edtCpf = (EditText)findViewById(R.id.cpf);
         maskCPF  = new MaskEditTextChangedListener("###.###.###-##", edtCpf);
-        edtCpf.setText("123");
         cpfCripto = edtCpf.getText().toString();
+        botao = (Button) findViewById(R.id.signin_button);
+
 
         edtCpf.addTextChangedListener(maskCPF);
         Criptografia criptografia = new Criptografia();
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpfAux = (edtCpf.getText().toString());
+                cpfAux = cpfAux.replace(".", "");
+                cpfAux = cpfAux.replace("-", "");
+                System.out.println("CPF " + cpfAux);
+            }
+        });
 
 
         String aidento = criptografia.bin2hex(criptografia.getHash(cpfCripto));
